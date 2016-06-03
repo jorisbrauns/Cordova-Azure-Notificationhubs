b444923c64a19f598a0a7514e6624379087c3c81
Joris Brauns <mr.joris@gmail.com>
2016-06-04 01:22:18 +0200

    Update AzureNotificationHubs.cs
    Cleanup

--- src/windows/AzureNotificationHubs/AzureNotificationHubs.cs
+++ src/windows/AzureNotificationHubs/AzureNotificationHubs.cs
@@ -1,18 +1,15 @@
-﻿using System;
+using System;
 using Windows.Networking.PushNotifications;
 using Microsoft.WindowsAzure.Messaging;
 using Windows.Foundation;
 using System.Threading.Tasks;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using Windows.UI.Popups;
 
 namespace AzureNotificationHubs
 {
     public sealed class AzureNotificationHubs
     {
 
-        private static TaskCompletionSource<string> levelCompletionSource = new TaskCompletionSource<string>();
+        private static TaskCompletionSource<string> completionSource = new TaskCompletionSource<string>();
 
         public static IAsyncOperation<string> register(string hubname, string connectionString)
         {
@@ -21,28 +18,28 @@ namespace AzureNotificationHubs
 
         private static async Task<string> RegisterNotificationHub(string hubname, string connectionString)
         {
-            levelCompletionSource = new TaskCompletionSource<string>();
+            completionSource = new TaskCompletionSource<string>();
 
             RegisterHub(hubname, connectionString);
 
-            return await levelCompletionSource.Task;
+            return await completionSource.Task;
         }
 
         private static async void RegisterHub(string hubname, string connectionString)
         {
             var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
 
-            
             var hub = new NotificationHub(hubname, connectionString);
             var result = await hub.RegisterNativeAsync(channel.Uri);
 
             // Displays the registration ID so you know it was successful
             if (result.RegistrationId != null)
             {
-                levelCompletionSource.SetResult(result.RegistrationId);
-            } else
+                completionSource.SetResult(result.RegistrationId);
+            }
+            else
             {
-                levelCompletionSource.SetResult("RegistrationId is null");
+                completionSource.SetResult("RegistrationId is null");
             }
         }
 
